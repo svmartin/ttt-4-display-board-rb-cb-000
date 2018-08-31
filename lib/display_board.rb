11e32a7ecb62f771aedcf3d8e4c290fdff7bebf5
Sean Martin <sean.martin@gmail.com>
2018-08-31 15:21:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,6 +9,6 @@ def display_board(board)
 end
 
 test_board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
-test_board = ["", "", "", "", "", "", "", "", ""]
+test_board2 = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-display_board(test_board)
+display_board(test_board2)
